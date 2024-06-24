90170cb3abd6d2fecc9c8835fc591ce91070a499
manur <manur@ManuBBXX.>
2024-06-24 14:26:18 +0200

    feat: add skeleton commit from astro blog tuto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind({
+    applyBaseStyles: true
+  }), mdx()],
+  output: "server",
+  adapter: vercel()
+});
